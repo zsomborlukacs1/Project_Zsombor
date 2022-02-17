2e89485131386928651d3c165c8e617a18082418
Lukács Zsombor <lukacszsombor11@gmail.com>
2022-02-17 15:10:36 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Demoproject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Demoproject.dir/main.c.obj"
+  "Demoproject.exe"
+  "Demoproject.exe.manifest"
+  "Demoproject.pdb"
+  "libDemoproject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Demoproject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Demoproject.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Demoproject.
+# This may be replaced when dependencies are built.
